04f7269426806a1f3a062368ab469ad7ff86c8c9
yxco <2523128454@qq.com>
2019-12-04 14:46:15 +0800

    更改部分配置信息

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   env: {
     node: true
   },
+  // test
   'extends': [
     'plugin:vue/essential',
     'eslint:recommended'
--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ module.exports = {
   presets: [
     '@vue/app'
   ],
+    // 懒加载
     plugins: [
         ['import', {
             libraryName: 'vant',
